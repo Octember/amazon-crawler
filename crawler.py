a43a621e92bf160598904f999223416f782273da
Noah Lindner <nwlindner@gmail.com>
2017-03-25 11:44:09 -0700

    Remove unecessary '&string'

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ from html.parser import HTMLParser
 import requests
 
 # This URL will surely not work forever, but at the time of writing it works
-AUTOCOMPLETE_URL = "https://completion.amazon.com/search/complete?method=completion&mkt=1&r=QHW0T16FVMD8GWM2WWM4&s=161-1591289-5903765&c=AWJECJG5N87M8&p=Detail&l=en_US&sv=desktop&client=amazon-search-ui&x=String&search-alias=aps&qs=&cf=1&fb=1&sc=1&q={}"
+AUTOCOMPLETE_URL = "https://completion.amazon.com/search/complete?method=completion&mkt=1&r=QHW0T16FVMD8GWM2WWM4&s=161-1591289-5903765&c=AWJECJG5N87M8&p=Detail&l=en_US&sv=desktop&client=amazon-search-ui&search-alias=aps&qs=&cf=1&fb=1&sc=1&q={}"
 
 def get_autocomplete_url(keyword):
     return AUTOCOMPLETE_URL.format(keyword)
